ed77bba63f37ac62da75cbbe505352822ec518bc
barbosadahora <103425559+barbosadahora@users.noreply.github.com>
2025-02-17 21:36:42 -0300

    Add files via upload

---
+++ src/main/java/br/ueg/desenvolvimento/web/projetodiego/ProjetodiegoApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.desenvolvimento.web.projetodiego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodiegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetodiegoApplication.class, args);
+	}
+
+}
